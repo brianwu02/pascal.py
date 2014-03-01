b8b15938208053470afa8eb4af1925e49747e209
Brian Wu <brianwu02@gmail.com>
2014-03-01 17:37:20 -0500

    add constants

---
+++ constants.py
@@ -0,0 +1,14 @@
+# Pascal Constants
+
+SYMBOLS = [
+    '<', '<>', '<<', ':', ':=', '>', '>>',
+    '<=', '>=', '-', '+', '*', '/', ';', ',', '[', ']', '(',
+    ')', '=', '^', '@', '(*'
+    ]
+
+RESERVED_WORDS = [
+    'program', 'var', 'begin', 'end', 'type',
+    'procedure', 'uses', 'function', 'for', 'while', 'repeat',
+    'do', 'then', 'downto', 'to','if', 'else', 'array', 'of',
+    'not', 'or',  'mod', 'and', 'const', 'div','record', 'exit'
+    ]
