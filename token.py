fbbdbbc9a3380b3de41fe38fb40f55f3f48eec06
Brian Wu <brianwu02@gmail.com>
2014-03-01 17:25:31 -0500

    adding accessor method to token class

--- token.py
+++ token.py
@@ -26,20 +26,45 @@ use LL parser style.
 class Token:
     """ A token object"""
     def __init__(self):
-        self.value = value
+        """all tokens must have these values"""
+        
+        self.current_tk = current_tk # not sure why this is needed yet.
+        self.current_tk_val = current_tk_val
+        self.current_name = current_name
+        self.current_file = current_file
         self.current_line = current_line
         self.current_column = current_column
+
         pass
 
     def typeof(self):
         """returns the type of token"""
         pass
 
-    def value(self):
+    def current_value(self):
         """returns the value of the token"""
-        return str(self.value)
+        return str(self.current_value)
+
+    def current_name(self):
+        """returns the current name of the token"""
+        return str(self.current_name)
+
+    def current_file(self):
+        """returns the current file the token is in.
+        does not make sense yet. multiple files i guess?"""
+        return str(self.current_file)
+
+    def current_line(self):
+        """returns the line of the token"""
+        return str(self.current_line)
+
+    def current_column(self):
+        """returns the current column of the token"""
+        return str(self.current_column)
+
 
     def __repr__(self):
+        """ prety print the token here"""
         pass
 
 class TK_INTLIT(Token):
