30a97cd291970004c2d1f949caaab2aedef3e5fc
Brian Wu <brianwu02@gmail.com>
2014-03-13 01:11:59 -0400

    change constants.py

--- modules/constants.py
+++ modules/constants.py
@@ -1,10 +1,29 @@
 # Pascal Constants
 
+# reserved_words: words which have a fixed meaning in language, cannot be changed or redefined
+# identifiers: names of symbols that the programmer defines
+# operators: symbols for mathematical or other operations
+# seperator: white space
+# constants: numerical or character constants used to denote actual values. 
+
+SYMBOLS = [
+        '+', '-', '*', '/' ,'=' '<', '>',
+        '[' , ']', '.', ',', '(', ')', ':',
+        '^', '@', '{', '}', '$', '#', '&',
+        '%', '<<' ,'>>','**', '<>', '><', 
+        '<=', '>=', ':=', '+=', '-=', '*=',
+        '/=', '(*', '*)', '(.', '.)', '//', 
+        
+        ';', '='
+        ]
+
+"""
 IDENTIFIERS = [
     '<', '<>', '<<', ':', ':=', '>', '>>',
     '<=', '>=', '-', '+', '*', '/', ';', ',',
     '[', ']', '(', ')', '=', '^', '@', '(*'
     ]
+"""
 
 RESERVED_WORDS = [
     'program', 'var', 'begin', 'end', 'type',
@@ -16,7 +35,6 @@ RESERVED_WORDS = [
 # http://www.freepascal.org/docs-html/ref/refsu1.html#x12-110001.3.1
 TURBO_PASCAL_RESERVED_WORDS = []
 
-# Identifiers are defined names for specific constants, types, variables, procedures,
-# units, and programs.
+# Identifiers are defined names for specific constants, types, variables, procedures, units, and programs.
 # All identifers consist between 1 and 127 significant characters (letters, digits,
 # and underscores), of which the first must be a letter (a-z or A-Z or _)
