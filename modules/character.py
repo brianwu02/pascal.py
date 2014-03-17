ff7f44bd062305e7887a5679832057e7892cb586
Brian Wu <brianwu02@gmail.com>
2014-03-16 20:12:04 -0400

    refactored character methods from tokenizer to character

--- modules/character.py
+++ modules/character.py
@@ -2,50 +2,121 @@
 class Character(object):
     """Character object that is fed to the tokenizer."""
 
-    def __init__(self, src_file):
-        self.src = src_fie
+    def __init__(self):
+        self.src = self.open_file()
         
         self.current_index = 0
         self.current_line = 1
-        self.data = self.src[0]
-        self.total_len = len(self.src)
+        self.data = self.src
 
-    def get_next_char(self):
+    
+    def is_done(self):
+        if self.current_index >= len(self.src):
+            return True
+        return False
+
+    def current_char(self):
         """returns the next charcter and increments index count."""
-        pass
+        c = self.data[self.current_index]
+        return c
 
-    def lookahead_char(self):
+    def get_current_char(self):
+        i = self.current_index
+        self.current_index += 1
+        c = self.data[i]
+        return c
+
+    def next_char(self):
         """returns the next next character. does not increment index count."""
-        pass
+        c = self.data[self.current_index + 1]
+        return c
 
-    def _increment_index(self):
-        pass
+    def is_current_alpha(self):
+        c = self.data[self.current_index]
+        return c.isalpha()
 
-    def _increment_line(self):
-        pass
+    def is_next_alpha(self):
+        c = self.data[self.current_index + 1]
+        return c.isalpha()
 
-    def is_char(self, char):
-        pass
+    def is_current_num(self):
+        try:
+            float(self.data[self.current_index])
+            return True
+        except ValueError:
+            return False
 
-    def is_num(self):
-        pass
+    def is_next_num(self):
+        try:
+            float(self.data[self.current_index + 1])
+            return True
+        except ValueError:
+            return False
 
     def is_identifier(self):
         pass
 
-    def is_whitespace(self):
-        pass
+    def is_current_whitespace(self):
+        if self.data[self.current_index] == " ":
+            return True
+        return False
 
-    def is_quote(self):
-        if self.data == "\'":
+    def is_next_whitespace(self):
+        if self.data[self.current_index + 1] == " ":
+            return True
+        return False
+
+    def is_current_newline(self):
+        if self.data[self.current_index] == "\n":
+            return True
+        return False
+
+    def is_next_newline(self):
+        if self.data[self.current_index + 1] == "\n":
+            return True
+        return False
+
+    def is_current_quote(self):
+        if self.data[self.current_index] == "\'":
             return True
         return False
 
 
+    def is_current_quote(self):
+        if self.data[self.current_index] == "\'":
+            return True
+        return False
+
+    def is_next_quote(self):
+        if self.data[self.current_index + 1] == "\'":
+            return True
+        return False
+
+
+    def _increment_index(self):
+        self.current_index += 1
+
+    def _increment_line(self):
+        self.current_line += 1
+
+
+
+    
+    def open_file(self):
+        with open('sample.ps', 'r') as f:
+            return f.read()
+
+
+    def print_index(self):
+        """debugging"""
+        return self.current_index
+
+
     def __repr__(self):
-        return ("char: %s\n ASCII: %s\n index: %s\n line: %s") % (
-                self.val, self.val, self.current_index, self. current_line
-                )
+        #return ("char: %s\n ASCII: %s\n index: %s\n line: %s") % (
+        #        self.val, self.val, self.current_index, self. current_line
+        #        )
+        return self.data[self.current_index]
 
 
 
