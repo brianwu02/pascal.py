a9dff9e78e9107a72d6ebcce94d597db6feed028
Brian Wu <brianwu02@gmail.com>
2014-04-18 01:56:10 -0400

    update todo.

--- modules/scanner.py
+++ modules/scanner.py
@@ -4,17 +4,23 @@ from character import Character
 from token import Token
 from tokenizer import Tokenizer
 
+# move this in to the wiki afterwards, leave it here for now.
 # TODO:
-# 1. RESERVED_WORD => RESERVED_WORD TOKEN
-#   - for each token object, determine whether it's a RESERVERD WORD.
+# 1. Tokenize everything.
+#   This process returns no errors. Tokenizer's only job is to return a 
+#   valid token. Only during the parsing phase will the compiler return syntax errors.
+# 
+#   a) RESERVED_WORD => RESERVED_WORD TOKEN
+#       - for each token object, determine whether it's a RESERVERD WORD.
 #
+
 """
-    RESERVED WORD LOGIC:
+RESERVED WORD LOGIC:
     - a token is reserved word when the token's string value matches
       a string from the reserved word list found in constants.py
-
-    -
 """
+
+
 if __name__ == "__main__":
     s = Tokenizer();
     for char in s:
