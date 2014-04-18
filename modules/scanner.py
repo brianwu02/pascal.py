c39553f2df13648199dde58172e77c3d2a831663
Brian Wu <brianwu02@gmail.com>
2014-04-18 01:52:58 -0400

    update TODO and reserverd word logic.

--- modules/scanner.py
+++ modules/scanner.py
@@ -4,7 +4,17 @@ from character import Character
 from token import Token
 from tokenizer import Tokenizer
 
+# TODO:
+# 1. RESERVED_WORD => RESERVED_WORD TOKEN
+#   - for each token object, determine whether it's a RESERVERD WORD.
+#
+"""
+    RESERVED WORD LOGIC:
+    - a token is reserved word when the token's string value matches
+      a string from the reserved word list found in constants.py
 
+    -
+"""
 if __name__ == "__main__":
     s = Tokenizer();
     for char in s:
