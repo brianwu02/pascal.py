a3668d75f6628cbf7b95d62b39271df4e8701cd8
Brian Wu <brianwu02@gmail.com>
2014-04-24 11:41:03 -0400

    add grammar rules file to be used by the scanner

---
+++ modules/grammar.py
@@ -0,0 +1,7 @@
+# holds all the grammar rules for pascal.
+# this algorithm uses the recursive descent where each non-terminal symbol
+# will have it's own parser function.
+
+
+if __name__ == "__main__":
+    pass
