8a1cf0464ffa83d333e0a99e9e5f11e8a71f2680
Brian Wu <brianwu02@gmail.com>
2014-04-25 16:35:27 -0400

    allow user to input pascal file as command line argument or fallback to default file.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from modules.scanner import Scanner
 from modules.parser import Parser
 
@@ -11,7 +12,21 @@ from modules.parser import Parser
     1. test that read correctly returns a character array
 """
 
-SOURCE_FILE_PATH = 'pascal_sample_code/addition.ps'
+# try to read source file from command line argument
+# if it does not exist, use a default pascal file. 
+default_source = 'pascal_sample_code/addition.ps'
+try:
+    if os.path.isfile(sys.argv[1]):
+        SOURCE_FILE_PATH = sys.argv[1]
+    else:
+        SOURCE_FILE_PATH = default_source
+        print("file: '%s' does not exist, using '%s' instead.") % (
+                sys.argv[1],
+                SOURCE_FILE_PATH
+                )
+except IndexError:
+    # default action when no argument is entered.
+    SOURCE_FILE_PATH = default_source
 
 def read(path):
     """returns a character array from source file."""
@@ -29,8 +44,9 @@ def run():
     parser = Parser()
 
 def test_run():
-    print read(SOURCE_FILE_PATH)
+    #print read(SOURCE_FILE_PATH)
+    print SOURCE_FILE_PATH
 
 if __name__ == "__main__":
     test_run()
-    run()
+    #run()
