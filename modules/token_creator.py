b53dff2bd99fee3a0630bc53a748d29d8b9a9181
Brian Wu <brianwu02@gmail.com>
2014-04-27 03:00:00 -0400

    add token_creator class file.

---
+++ modules/token_creator.py
@@ -0,0 +1,10 @@
+from token import Token
+
+# probably does not need to be a class could simply be a module
+# recieves a token string and attempts to parse the token string
+# and return a token object
+
+class TokenCreator:
+
+    def __init__(self):
+        pass
