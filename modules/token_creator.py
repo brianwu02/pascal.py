f82dc08d5f0268c42c9bca1138c384beb46468a1
Brian Wu <brianwu02@gmail.com>
2014-04-27 18:16:48 -0400

    remove incorrect comment. state is required to be saved so it DOES need to be a class. previous and current token are required. not sure about next.

--- modules/token_creator.py
+++ modules/token_creator.py
@@ -1,9 +1,5 @@
 from token import Token
 
-# probably does not need to be a class could simply be a module
-# recieves a token string and attempts to parse the token string
-# and return a token object
-
 class TokenCreator:
 
     # design patterns say I should require my dependencies in the constructor
@@ -11,11 +7,21 @@ class TokenCreator:
     # is called so what do I do?! Probably didnt need a class for this...
     
     def __init__(self):
+        self.current_token = None
+        self.previous_token = None
+        self.next_token = None
         pass
 
-    def create(self, token_string):
+    def create(self, token_tuple):
         """method called by the scanner. takes a tokenizer string value
         as input and returns a token object."""
         token = Token()
+
+        self._parse_token(token_tuple)
+
+        return token
+
+    def _parse_token(self):
+        pass
 
 
