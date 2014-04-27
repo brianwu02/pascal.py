0603c996e78789f04dcced162e96a02e116a4b20
Brian Wu <brianwu02@gmail.com>
2014-04-27 18:02:42 -0400

    add empty scanner tests.

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -17,9 +17,79 @@ class TestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_reserved_word_token(self):
+    def test_assignment_operator(self):
+        pass
+    
+    def test_addition_operator(self):
+        pass
+
+    def test_subtraction_operator(self):
+        pass
+
+    def test_multiplication_operator(self):
+        pass
+
+    def test_division_operator(self):
+        pass
+
+    def test_equal_operator(self):
+        pass
+
+    def test_unequal_operator(self):
+        pass
+
+    def test_less_than_operator(self):
+        pass
+    
+    def test_greater_than_operator(self):
+        pass
+
+    def test_less_than_or_equal_to_operator(self):
+        pass
+
+    def test_greater_than_or_equal_to_operator(self):
+        pass
+
+    def test_identifier(self):
+        pass
+
+    def test_string_literal(self):
+        pass
+
+    def test_integer_literal(self):
+        pass
+
+    def test_real_literal(self):
+        pass
+
+    def test_keyword(self):
+        pass
+
+    def test_EOF(self):
+        pass
+
+    def test_comment_string(self):
+        pass
+
+    def test_reserved_word_if(self):
         """test that scanner returns a correct token object given
         a reserverd_word tuple."""
-
         pass
+
+    def test_reserved_word_var(self):
+        pass
+
+    def test_reserved_word_else(self):
+        pass
+
+    def test_reserved_word_begin(self):
+        pass
+
+    def test_reserved_word_end(self):
+        pass
+
+    def test_reserved_word_while(self):
+        pass
+
+
 
