2f34a9bd7d48c3cf03f925fc430d28d7d286cf22
Brian Wu <brianwu02@gmail.com>
2014-04-29 18:59:23 -0400

    add production rules.

--- modules/grammar.py
+++ modules/grammar.py
@@ -1,6 +1,21 @@
-# holds all the grammar rules for pascal.
-# this algorithm uses the recursive descent where each non-terminal symbol
-# will have it's own parser function.
+# holds all the grammar rules for pascal. this algorithm uses the 
+# recursive descent where each non-terminal symbol will have it's own parser function.
+
+def identifier_rule(word):
+    """returns True if word is an identifier that follows this grammar:
+    identifier  ->  letter
+                ->  letter | digit
+    """
+
+def _letter_rule(char):
+    """a letter is a terminal symbol.
+    """
+    pass
+
+def _digit_rule(char):
+    """a digit is a terminal symbol.
+    """
+    pass
 
 
 if __name__ == "__main__":
