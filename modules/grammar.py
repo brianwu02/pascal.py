06de0676d840a379d024190df394033fd88572d1
Brian Wu <brianwu02@gmail.com>
2014-04-30 20:36:18 -0400

    add some grammar rules I need to implement.

--- modules/grammar.py
+++ modules/grammar.py
@@ -1,19 +1,60 @@
 # holds all the grammar rules for pascal. this algorithm uses the 
 # recursive descent where each non-terminal symbol will have it's own parser function.
 
-def identifier_rule(word):
+
+def CompilationUnit(token):
+    """ CompilationUnit -> ProgramModule
+    """
+    pass
+
+def ProgramModule(token):
+    """ ProgramModule   -> program ident ProgramParamters ';' Block '.'
+    """
+    pass
+
+def block(token):
+    """ Block   -> [Declarations] StatementSequence
+                | [ConstantDefBlock]
+                | [TypeDefBlock]
+                | [VariableDeclBlock]
+                | [SubProgDeclList]
+    """
+    pass
+
+def ConstantDefBlock(token):
+    pass
+
+def VariableDeclBlock(token):
+    """
+    var -> VariableDecl ';' { VariableDecl ';' }
+    """
+    pass
+
+def identifier_rule(token):
     """returns True if word is an identifier that follows this grammar:
     identifier  ->  letter
                 ->  letter | digit
     """
-
-def _letter_rule(char):
-    """a letter is a terminal symbol.
-    """
     pass
 
-def _digit_rule(char):
-    """a digit is a terminal symbol.
+def _letter_rule(token):
+    """a letter is a terminal symbol."""
+    pass
+
+def _digit_rule(token):
+    """a digit is a terminal symbol."""
+    pass
+
+def factor():
+    """
+    Factor  -> number
+            | string | true | false | nil
+            | Designator
+            | '(' Expression ')'
+            | not Factor
+            | Setvalue
+            | FunctionCall
+
     """
     pass
 
