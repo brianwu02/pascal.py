926481b88404e861811cb0c5d8d5b121245e4f5b
Brian Wu <brianwu02@gmail.com>
2014-05-04 21:58:49 -0400

    add tests for token attributes. e.g. is_mult, is_add, is_relation, is_unary.

---
+++ tests/test_tokens.py
@@ -0,0 +1,12 @@
+import unittest
+from ..modules.token_creator import TokenCreator
+from ..modules.pascal_token import Token 
+
+class TestTokenAttributes(unittest.TestCase):
+    """test that attribute methods return correct boolean values"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
