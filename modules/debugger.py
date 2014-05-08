ca2c66bed57b6c60f09bc3946ebad3e3b63638b0
Brian Wu <brianwu02@gmail.com>
2014-05-07 20:49:33 -0400

    update the debugger to accept token object format.

--- modules/debugger.py
+++ modules/debugger.py
@@ -4,20 +4,8 @@ class DebugPrinter:
     def __init__(self):
         pass
 
-    def print_debug(self, s_dict):
-        expected_tk_type = s_dict['expected_tk_type']
-        total_tokens = s_dict['total_tokens']
-        current_tk_count = s_dict['current_tk_count']
-        got_tk_val = s_dict['got_tk_val']
-        got_tk_type = s_dict['got_tk_type']
-        got_tk_cnt = s_dict['got_tk_cnt']
-        got_tk_name = s_dict['got_tk_name']
-        got_tk_creation_state = s_dict['got_tk_creation_state']
-        got_line = s_dict['got_line']
-        got_l_ind = s_dict['got_l_ind']
-        line_info = s_dict['line_info']
-        parse_state = s_dict['parse_state']
-
+    def print_debug(self, token, tk_index, tk_count):
+        line_info = "(%s, %s)" % (token.get_line_number(), token.get_line_index())
         debug_msg = """
         ------- token %s out of %s parsed -------
         parse_state : %s
@@ -26,38 +14,40 @@ class DebugPrinter:
         name        : %s
         line info   : %s
         """ % (
-                current_tk_count,
-                total_tokens,
-                parse_state,
-                got_tk_type,
-                got_tk_val,
-                got_tk_name,
+                tk_index,
+                tk_count,
+                token.get_creation_state(),
+                token.get_type(),
+                token.get_value(),
+                token.get_name(),
                 line_info
                 )
         print(debug_msg)
 
-    def tk_match_err(self):
-        pass
-
-
-    def _debug_message(self):
-        
-        tk_match_err_msg = """TK_MATCH_ERR
-        ---tk_match_err on token %s out of %s----
-        --> expected match  : %s in parse_state: %s
-        --> got type        : %s
-        got value   : %s
-        got name    : %s
-        got line    : %s
-        create state: %s
+    def raise_match_tk_err(self, token, tk_index, tk_count, expected_tk, got_tk):
+        line_info = "(%s, %s)" % (token.get_line_number(), token.get_line_index())
+        tk_match_err = """ TK_MATCH_ERR
+        ------- token %s out of %s -------
+        ---> EXPECTED_MATCH : %s
+        ___> GOT_TK_TYPE    : %s
+        parse_state : %s
+        type        : %s
+        value       : %s
+        name        : %s
+        line info   : %s
         """ % (
-                current_tk_cnt,
-                total_tokens,
-                expected_tk_type,
-                parse_state,
-                got_tk_type,
-                got_tk_val,
-                got_tk_name,
-                line_info,
-                got_tk_create_state
+                tk_index,
+                tk_count,
+                expected_tk,
+                got_tk,
+                token.get_creation_state(),
+                token.get_type(),
+                token.get_value(),
+                token.get_name(),
+                line_info
                 )
+        raise Exception(tk_match_err)
+
+
+    
+
