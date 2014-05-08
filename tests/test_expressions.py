64a9819f5094894ea7f4c278a41bb11d4c95534a
Brian Wu <brianwu02@gmail.com>
2014-05-08 12:45:40 -0400

    add expressions stub tests.

---
+++ tests/test_expressions.py
@@ -0,0 +1,25 @@
+import unittest
+from ..modules.parser import Parser
+
+class TestExpressions(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_addition_expression(self):
+        """x := 5 + 5"""
+        pass
+
+    def test_subtraction_expression(self):
+        """x := 5 - 5"""
+        pass
+
+    def test_division_expression(self):
+        """x := 5 / 5"""
+        pass
+    
+    def test_multiplication_expression(self):
+        """x := 5 * 5"""
+        pass
