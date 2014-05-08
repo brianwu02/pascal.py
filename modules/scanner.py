2e429bff2b996a5e8b6e91b57d22bf5f2ab35a88
Brian Wu <brianwu02@gmail.com>
2014-05-08 15:08:42 -0400

    docstring cleanup

--- modules/scanner.py
+++ modules/scanner.py
@@ -2,29 +2,12 @@ from character_scanner import CharacterScanner
 from token_creator import TokenCreator
 
 # REMEMBER:
-#
 # context-free: subsitution of the symbols left of the '=' by a sequence derived from
 # the expression right of the '=' is always permitted regardless of the context embedded
 # witin the sentence.
-#
 # Scanning => Lexical Analysis: takes characters as input where the syntax is regular.
 # Parsing  => Syntax Analysis: takes symbols as input where the syntax is context free.
-#
-# move this in to the wiki afterwards, leave it here for now.
-# TODO:
-# 1. Tokenize everything.
-#   This process returns no errors. Tokenizer's only job is to return a 
-#   valid token. Only during the parsing phase will the compiler return syntax errors.
-# 
-#   a) RESERVED_WORD => RESERVED_WORD TOKEN
-#       - for each token object, determine whether it's a RESERVERD WORD.
-#
 
-"""
-RESERVED WORD LOGIC:
-    - a token is reserved word when the token's string value matches
-      a string from the reserved word list found in constants.py
-"""
 class Scanner:
     """This is the Lexical Analyzer.
     It takes characters as input and outputs a stream of tokens.
@@ -44,11 +27,6 @@ class Scanner:
         character_scanner = self.character_scanner
         tk_creator = self.token_creator
         tk_list = self.token_list
-
-        # iterate over token strings in tokenizer and
-        # append the token object to tk_list
-        #for tuple_val in tokenizer:
-        #    tk_list.append(tk_creator.create(tuple_val))
 
         [tk_list.append(tk_creator.create(tuple_val)) for tuple_val in character_scanner]
 
