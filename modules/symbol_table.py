25088d12290a049f999a9357b3b6095428143913
Brian Wu <brianwu02@gmail.com>
2014-05-13 12:18:49 -0400

    parse variable declaration method works. correctly adds to symbol table.

--- modules/symbol_table.py
+++ modules/symbol_table.py
@@ -13,6 +13,26 @@ class SymbolTable:
         #   }
         self.table = {}
 
+    def parse_variable_declaration(self, symbol_array):
+        """pretty hacky. sends over a list of tokens where (first, last-1) are
+        identifiers and last is type."""
+        # loop over all variable identifers and add to identifiers list.
+        identifiers = []
+        ident_type = None
+        type_index = len(symbol_array) - 1
+        for index, token in enumerate(symbol_array):
+            if index == type_index:
+                # the way it's coded, the last element in array MUST be the type or something breaks.
+                ident_type = token.get_value()
+            else:
+                identifiers.append(token)
+
+        # loop over identifiers and add them to the symbol table
+        for ident in identifiers:
+            self.table[ident] = {'type': ident_type }
+
+
+
     def add(self, token):
         """adds identifier to symbol table"""
         ident_name = token.get_value()
