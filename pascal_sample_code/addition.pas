acd8f9cceadd2a6ea0541fecc60c4faee2178b5b
Brian Wu <brianwu02@gmail.com>
2014-05-20 14:54:46 -0400

    update expression examples

--- pascal_sample_code/addition.pas
+++ pascal_sample_code/addition.pas
@@ -1,8 +1,9 @@
 program addNumbers;
 
-Var a,b : Integer;
-Var c: Integer;
+Var a, b, c : Integer;
 
 begin
-    a := 1 + 1;
+    a := (1 + 2) * (3 - 2);
+    b := ((2 * 2 * 2) + (2 * 2 * 2)) + 2; 
+    c := (1 * 2) + (1 + 1) - (10 * 2);
 end.
